92452da7939976d5ac1e9c1f07378beac5d3b81f
Diego <diegogonzalez893@gmail.com>
2025-02-05 20:48:21 -0300

    Actualizo juego

---
+++ app.js
@@ -0,0 +1,54 @@
+let amigos = [];
+console.log(amigos);
+
+function recorrerLista (){
+    let listaAmigosHTML = document.getElementById('listaAmigos');
+    listaAmigosHTML.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        let nuevoElemento = document.createElement('li');
+        nuevoElemento.textContent = amigos[i];
+        listaAmigosHTML.appendChild(nuevoElemento);
+    }
+}
+
+function asignarTextoElemento(elemento,texto){
+    let elementohtml = document.querySelector(elemento);
+    elementohtml.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+    if (nombreAmigo == "") {
+        alert("Por favor, inserte un nombre.");
+    } else if (amigos.includes(nombreAmigo)){
+        alert("El nombre ya se encuentra en la lista");
+    }else {
+        amigos.push(nombreAmigo);
+        console.log(amigos);         
+        recorrerLista();
+        limpiarCaja();
+    }
+    return
+}
+
+function sortearAmigo(){
+    if (amigos.length < 2){
+        alert("Debe agregar al menos 2 amigos");
+    } else {
+    let amigoSeleccionado = amigos[Math.floor(Math.random()*amigos.length)];
+    asignarTextoElemento("#resultado",`El amigo secreto es: ${amigoSeleccionado}`)
+    amigos = amigos.filter(amigo => amigo !== amigoSeleccionado);
+    recorrerLista();
+    }
+    return;
+
+
+}
+
+
+
+
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
